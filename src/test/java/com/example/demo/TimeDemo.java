450f7f86b8e4f85b81f5267772118d6b2a5fb735
xueyou.wu <xueyou.wu@langtaojin.com>
2018-09-18 01:43:38 +0800

    添加jodatime

---
+++ src/test/java/com/example/demo/TimeDemo.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.joda.time.DateTime;
+import org.joda.time.Days;
+
+import javax.sound.midi.Soundbank;
+
+/**
+ * Created by wuxueyou on 2018/9/18.
+ */
+public class TimeDemo {
+    public static void main(String[] args) {
+        System.out.println(123);
+        DateTime dateTime = new DateTime();
+        System.out.println(dateTime.toString("YYYY-MM-dd HH:mm:ss.SSS"));
+        System.out.println(dateTime.getMillis());
+        DateTime dateTime2 = dateTime.plusDays(2);
+        System.out.println(dateTime2.toString("YYYY-MM-dd HH:mm:ss.SSS"));
+        System.out.println(Days.daysBetween(dateTime2, dateTime).getDays());
+    }
+}
