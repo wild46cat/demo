b809ee594286ed97a126cde73f69789bcd87df91
xueyou.wu <xueyou.wu@langtaojin.com>
2019-01-16 18:57:09 +0800

    add some

--- src/test/java/com/example/demo/TimeDemo.java
+++ src/test/java/com/example/demo/TimeDemo.java
@@ -1,9 +1,11 @@
 package com.example.demo;
 
 import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
 import org.joda.time.Days;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 
-import javax.sound.midi.Soundbank;
 
 /**
  * Created by wuxueyou on 2018/9/18.
@@ -12,13 +14,23 @@ import javax.sound.midi.Soundbank;
 public class TimeDemo {
     public static void main(String[] args) {
         System.out.println(123);
+//      日期to字符串
         DateTime dateTime = new DateTime();
         System.out.println(dateTime.toString("YYYY-MM-dd HH:mm:ss.SSS"));
         System.out.println(dateTime.getMillis());
+//        日期加减
         DateTime dateTime2 = dateTime.plusDays(2);
         System.out.println(dateTime.toString("YYYY-MM-dd HH:mm:ss.SSS"));
         System.out.println(dateTime.getMillis());
         System.out.println(dateTime2.toString("YYYY-MM-dd HH:mm:ss.SSS"));
         System.out.println(Days.daysBetween(dateTime2, dateTime).getDays());
+//        字符串to日期
+        String dateStr = "2018-12-12 11:12:13.333";
+        DateTimeFormatter fmt = DateTimeFormat.forPattern("YYYY-MM-dd HH:mm:ss.SSS");
+        DateTime dateTime3 = DateTime.parse(dateStr, fmt);
+        System.out.println(dateTime3);
+
+        DateTime dateTime1 = new DateTime(57620000);
+        System.out.println(dateTime1.toString("mm:ss"));
     }
 }
