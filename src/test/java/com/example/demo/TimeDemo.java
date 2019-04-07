627d6587abbe77e5922a6105bc1038f663958207
xueyou.wu <xueyou.wu@langtaojin.com>
2019-04-07 22:15:06 +0800

    add global handle UncaughtException

--- src/test/java/com/example/demo/TimeDemo.java
+++ src/test/java/com/example/demo/TimeDemo.java
@@ -1,7 +1,6 @@
 package com.example.demo;
 
 import org.joda.time.DateTime;
-import org.joda.time.DateTimeZone;
 import org.joda.time.Days;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
@@ -27,12 +26,26 @@ public class TimeDemo {
         System.out.println(dateTime2.toString("YYYY-MM-dd HH:mm:ss.SSS"));
         System.out.println(Days.daysBetween(dateTime2, dateTime).getDays());
 //        字符串to日期
-        String dateStr = "2018-12-12 11:12:13.333";
+        String dateStr = "2018-12-12 00:12:13.333";
         DateTimeFormatter fmt = DateTimeFormat.forPattern("YYYY-MM-dd HH:mm:ss.SSS");
         DateTime dateTime3 = DateTime.parse(dateStr, fmt);
         System.out.println(dateTime3);
+        System.out.println(dateTime3.getHourOfDay());
 
         DateTime dateTime1 = new DateTime(57620000);
         System.out.println(dateTime1.toString("mm:ss"));
+        System.out.println("aaa=====");
+        String dateStr1 = "2019-04-02";
+        String dateStr2 = "2019-04-09";
+        System.out.println(daysBetweenOverMax(dateStr1, dateStr2, 7));
+        System.out.println(String.format("%.2f",100 * 0.0007315));
+    }
+
+    public static boolean daysBetweenOverMax(String dateStrStart, String dateStrEnd, int max) {
+        DateTimeFormatter fmt2 = DateTimeFormat.forPattern("yyyy-MM-dd");
+        DateTime dateTimeStart = DateTime.parse(dateStrStart, fmt2);
+        DateTime dateTimeEnd = DateTime.parse(dateStrEnd, fmt2);
+        return Math.abs(Days.daysBetween(dateTimeStart, dateTimeEnd).getDays()) > max;
+
     }
 }
